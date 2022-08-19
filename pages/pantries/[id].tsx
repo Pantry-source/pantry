f3afc17f3cbce9d973c7f382369f1b4742660944
Lana Nelson <shvetusya@gmail.com>
2022-08-19 15:08:55 -0700

    show list of products

--- pages/pantries/[id].tsx
+++ pages/pantries/[id].tsx
@@ -7,7 +7,9 @@ import SlideOver from '../../components/SlideOverDialog';
 export default function Pantry() {
   const [pantry, setPantry] = useState(null);
   const [categories, setCategories] = useState(null);
+  const [categoriesMap, setCategoriesMap] = useState(null);
   const [units, setUnits] = useState(null);
+  const [unitsMap, setUnitsMap] = useState(null);
   const [currentProduct, setCurrentProduct] = useState({});
   const [isAddingProducts, setIsAddingProducts] = useState(false)
   function onProductChange(e) {
@@ -36,12 +38,24 @@ export default function Pantry() {
       .from('categories')
       .select(`*`);
     setCategories(data);
+    setCategoriesMap(data?.reduce((previous, category) => {
+      return {
+        ...previous,
+        [category.id]: category.name
+      };
+    }, {}));
   }
   async function fetchQuantityUnits() {
     const { data } = await supabase
       .from('quantity_units')
       .select(`*`);
     setUnits(data);
+    setUnitsMap(data?.reduce((previous, category) => {
+      return {
+        ...previous,
+        [category.id]: category.name
+      };
+    }, {}));
   }
 
   async function saveCurrentProduct(e) {
@@ -59,27 +73,73 @@ export default function Pantry() {
     fetchQuantityUnits();
   }, [id])
   if (!pantry) return null;
-  const { description, title, user_email } = pantry;
+  const { description, title, products } = pantry;
   function addProducts() {
     setIsAddingProducts(true);
   }
+
   return (
     <div>
       <div className="px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">{title}</h1>
-          <p className="mt-2 text-sm text-gray-700">{description}</p>
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold text-gray-900">{title}</h1>
+            <p className="mt-2 text-sm text-gray-700">{description}</p>
+          </div>
+          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <button
+              type="button"
+              onClick={addProducts}
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:w-auto">
+              Add Products
+            </button>
+          </div>
         </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            onClick={addProducts}
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:w-auto">
-            Add Products
-          </button>
-        </div>
-      </div>
+        <table className="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
+              >
+                Product
+              </th>
+              <th
+                scope="col"
+                className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+              >
+                Is essential
+              </th>
+              <th
+                scope="col"
+                className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+              >
+                Expires
+              </th>
+              <th
+                scope="col"
+                className="py-3.5 pl-3 pr-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0"
+              >
+                Vendor
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id} className="border-b border-gray-200">
+                <td className="py-4 pl-4 pr-3 text-sm sm:pl-6 md:pl-0">
+                  <div className="font-medium text-gray-900">{product.name}</div>
+                  <div className="mt-0.5 text-gray-500">
+                    {product.quantity_amount} {unitsMap && unitsMap[product.quantity_unit]}
+                  </div>
+                </td>
+                <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{ product.is_essential ? 'yes' : 'no' }</td>
+                <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{ product.expires_at || 'not specified' }</td>
+                <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{product.vendor || ''}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <SlideOver 
         open={isAddingProducts} 
