2f15fd057fb17a023431e2031beb68d61eb294c4
jf <jpf0628@gmail.com>
2022-09-07 15:45:58 -0700

    FIXED BUG categoryId was never set if user left Produce selected by default. User now has to select category

--- components/ProductEditor.tsx
+++ components/ProductEditor.tsx
@@ -1,11 +1,18 @@
 export default function ProductEditor({ categories, units, onProductChange, product }) {
   const unitOptions = units.map(unit => <option value={unit.id} key={unit.id}>{unit.name}</option>);
   const categoryOptions = categories.map(category => <option value={category.id} key={category.id}>{category.name}</option>);
-  const defaultUnitId = units[0].id, defaultCategory = categories[0].id;
+  const defaultUnitId = units[0].id, defaultCategory = undefined;
+  //below, defaultCategory is only used initially to set the category_id(this sets the number 1 not "1" and value on line 112 is still null)
+  //now users need to explicitly select their category. 
+  //defaultCategory = categories[0].id
+  console.log('categories[0]', categories[0].id)
   const { name = '', quantity_amount = '', quantity_unit = defaultUnitId, category_id = defaultCategory, vendor = '' } = product;
+  console.log("defaultCategory", defaultCategory)
+  console.log('defaultUnitId', defaultUnitId)
+  console.log('cat_id', category_id)
   return (
     <div className="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
-      
+
       {/* Product name */}
       <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
         <div>
@@ -72,7 +79,7 @@ export default function ProductEditor({ categories, units, onProductChange, prod
             onChange={onProductChange}
             id="amount"
             className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
-            placeholder="0"/>
+            placeholder="0" />
           <div className="absolute inset-y-0 right-0 flex items-center">
             <label htmlFor="unit" className="sr-only">
               Unit
@@ -105,7 +112,8 @@ export default function ProductEditor({ categories, units, onProductChange, prod
             value={category_id}
             onChange={onProductChange}
             className="w-full pl-3 border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-              {categoryOptions}
+            <option value="null">Select Category</option>
+            {categoryOptions}
           </select>
         </div>
       </div>
