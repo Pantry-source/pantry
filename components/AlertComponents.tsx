99373de59c5cd8eb73315e5a5bce56ec7a9df5df
jf <jpf0628@gmail.com>
2022-09-20 17:48:14 -0700

    fixing unescaped quote

--- components/AlertComponents.tsx
+++ components/AlertComponents.tsx
@@ -1,18 +1,14 @@
 //can't find tailwind module
-// import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
 
 export function AlertFormError() {
   return (
     <div className="rounded-md bg-yellow-50 p-4">
       <div className="flex">
-        {/* <div className="flex-shrink-0">
-          <ExclamationTriangleIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
-        </div> */}
         <div className="ml-3">
           <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
           <div className="mt-2 text-sm text-yellow-700">
             <p>
-              null value in column "name" of relation "products" violates not-null constraint
+            null value in column "name" of relation "products" violates not-null constraint
             </p>
           </div>
         </div>
@@ -22,15 +18,11 @@ export function AlertFormError() {
 }
 
 /* This example requires Tailwind CSS v2.0+ */
-// import { XCircleIcon } from '@heroicons/react/20/solid'
 
 export function AlertFormList() {
   return (
     <div className="rounded-md bg-red-50 p-4">
       <div className="flex">
-        {/* <div className="flex-shrink-0">
-          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
-        </div> */}
         <div className="ml-3">
           <h3 className="text-sm font-medium text-red-800">There were 2 errors with your submission</h3>
           <div className="mt-2 text-sm text-red-700">
