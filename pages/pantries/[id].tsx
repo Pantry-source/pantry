1407e9cf210b343ce0533aad6be46040e6f66098
jf <jpf0628@gmail.com>
2022-10-04 13:40:04 -0700

    category filter shows dynamic categories available, and filters

--- pages/pantries/[id].tsx
+++ pages/pantries/[id].tsx
@@ -147,7 +147,7 @@ export default function Pantry() {
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                  <Filter />
+                  <Filter categories={categories.filter(category => category.products)}/>
                 <table className="min-w-full">
                   <thead className="bg-white">
                     <tr>
@@ -213,7 +213,7 @@ export default function Pantry() {
         open={isAddingProducts}
         onClose={() => setIsAddingProducts(false)}
         onSubmit={saveCurrentProduct}
-        title="New product"
+         title="New product"
         subtitle={`Fillout the information below to add a product to ${title}`}>
         <ProductEditor
           product={currentProduct}
