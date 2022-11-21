e0be27daa82336020c6786670222e1a7087f6900
jf <jpf0628@gmail.com>
2022-11-21 15:01:14 -0800

    added styling to button

--- components/PillButton.tsx
+++ components/PillButton.tsx
@@ -7,7 +7,7 @@ function PillButton({ id, quantity, updateQuantity }) {
 
   function increment() {
     setAmount(amount => amount + 1);
-    updateQuantity(id,amount)
+    updateQuantity(id, amount)
   }
 
   function decrement() {
@@ -16,22 +16,22 @@ function PillButton({ id, quantity, updateQuantity }) {
   }
 
   return (
-    <div className='rounded-full' style={{ backgroundColor: 'transparent', border: 'solid' }}>
+    <div className='rounded-full' style={{ backgroundColor: 'transparent', border: '1px solid rgba(0, 0, 0, 0.05)' }}>
       <button
         onClick={decrement}
         type="button"
-        className="h-5 w-5 inline-flex items-center rounded-full border border-transparent  p-5  shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        className="h-5 w-5 hover:text-black inline-flex items-center font-100 rounded-full p-5 shadow-sm hover:font-bold focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         -
       </button>
       <button
         type="button"
-        className="h-5 w-5 inline-flex items-center border-transparent  p-5  shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        className="h-5 w-5 hover:text-black inline-flex items-center p-5 shadow-sm hover:font-bold focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         1
       </button>
       <button
         onClick={increment}
         type="button"
-        className="h-5 w-5 inline-flex items-center rounded-full border border-transparent  p-5  shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        className="h-5 w-5 hover:text-black inline-flex items-center rounded-full p-5 shadow-sm hover:font-bold focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         +
       </button>
     </div>
