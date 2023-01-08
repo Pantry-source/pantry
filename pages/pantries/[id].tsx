1020735d4f7e6cc5cc21a4da631b0e81a485acf5
jf <jpf0628@gmail.com>
2023-01-08 12:14:28 -0800

    fixed ReferenceError

--- pages/pantries/[id].tsx
+++ pages/pantries/[id].tsx
@@ -391,7 +391,6 @@ export default function Pantry() {
                               {selectedProduct.includes(product) && (
                                 <div className="absolute inset-y-0 left-0 w-0.5 bg-indigo-600" />
                               )}
-                              {cl('product', product)}
                               <input
                                 type="checkbox"
                                 className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 sm:left-6"
