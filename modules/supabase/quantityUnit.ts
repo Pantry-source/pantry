b29138776b7276579cadede217eb14c616c69734
Svetlana Nelson <shvetusya@gmail.com>
2023-04-18 14:03:54 -0700

    Fix typescript errors in all pages

--- modules/supabase/quantityUnit.ts
+++ modules/supabase/quantityUnit.ts
@@ -2,7 +2,7 @@ import supabase from '../../api';
 import type { Database } from '../../types/generated/supabase';
 
 export type QuantityUnit = Database['public']['Tables']['quantity_units']['Row'];
-export type QuantityUnitMap = Map<number, string> | {};
+export type QuantityUnitMap = Record<QuantityUnit['id'], QuantityUnit['name']>;
 
 let QuantityUnitMapTest: QuantityUnitMap = { 123: 'test'};
 
