33132ee25969500d23afb4b7ec2a99d95496ccba
Svetlana Nelson <shvetusya@gmail.com>
2023-09-02 12:18:38 -0700

    custom hooks for recipes

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -6,25 +6,23 @@ import EmptyState from '../../components/EmptyState';
 import RecipeBrowser from '../../components/RecipeBrowser';
 import { type Recipe, fetchAll } from '../../modules/supabase/recipe';
 import RecipeEditor from '../../components/RecipeEditor';
+import useRecipes from '../../hooks/useRecipes';
 
 export default function Recipes() {
   const supabaseClient = useSupabaseClient();
   const { isLoading, session, error } = useSessionContext();
-  const [ recipes, setRecipes ] = useState<Recipe[]>();
   const [isRecipeEditorOpen, setIsRecipeEditorOpen] = useState(false);
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe>();
   const user = useUser();
-  async function fetchRecipes() {
-    const { error, data } = await fetchAll();
-    if (!error && data) {
-      setRecipes(data);
-    }
+  const [recipes, fetchRecipes, recipesLoading, recipesLoadingError] = useRecipes(user);
+
+  if (isLoading || recipesLoading) {
+    return null;
   }
-  useEffect(() => {
-    if (user) {
-      fetchRecipes();
-    }
-  }, [user]);
+
+  if (error) {
+    console.error(error);
+  }
 
   if (!session) {
     return (
@@ -37,37 +35,47 @@ export default function Recipes() {
       />
     );
   }
-  if (!recipes?.length) {
-    return (
-      <EmptyState
+  const viewRecipe = (recipe: Recipe) => {
+    setSelectedRecipe(recipe);
+    setIsRecipeEditorOpen(true);
+  }
+  const createRecipe = () => {
+    setSelectedRecipe(undefined);
+    setIsRecipeEditorOpen(true);
+  }
+
+  const renderRecipes = () => {
+    if (recipes && recipes.length) {
+      return (
+        <>
+          <div className='flex justify-between items-center'>
+          <h2 className="text-sm font-medium text-gray-500 mt-10 mb-5">All Recipes</h2>
+            <button type="button"
+              onClick={createRecipe}
+              className="ml-6 rounded-md px-3.5 py-2.5 bg-cyan-600 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
+              Add recipe
+            </button>
+          </div>
+          <div className='flex-1'>
+            <RecipeBrowser recipes={recipes} onRecipeViewClick={viewRecipe}/>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <EmptyState
           primaryAction="Add"
           onPrimaryActionClick={createRecipe}
           header={"Recipes"}
           subheading={"Start by adding a recipe"}
         />
-    )
-  }
-  function viewRecipe(recipe: Recipe) {
-    setSelectedRecipe(recipe);
-    setIsRecipeEditorOpen(true);
-  }
-  function createRecipe() {
-    setSelectedRecipe(undefined);
-    setIsRecipeEditorOpen(true);
-  }
+      );
+    }
+  };
+  
   return (
     <div className='h-full flex flex-col'>
-      <div className='flex justify-between items-center'>
-        <h2 className="text-sm font-medium text-gray-500 mt-10 mb-5">All Recipes</h2>
-        <button type="button"
-          onClick={createRecipe}
-          className="ml-6 rounded-md px-3.5 py-2.5 bg-cyan-600 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
-          Add recipe
-        </button>
-      </div>
-      <div className='flex-1'>
-        <RecipeBrowser recipes={recipes} onRecipeViewClick={viewRecipe}/>
-      </div>
+      {renderRecipes()}
       <RecipeEditor recipe={selectedRecipe} open={isRecipeEditorOpen} onClose={() => {setIsRecipeEditorOpen(false); fetchRecipes()}} />
     </div>
   );
